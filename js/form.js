0942cb03c19f3cfd7beeb289da175339822ee274
CendresChaudes <romqaaa1337@gmail.com>
2023-04-06 14:39:51 +0300

    feat: добавляет модуль, отвечающий за формы

---
+++ js/form.js
@@ -0,0 +1,34 @@
+const newAdvertisementForm = document.querySelector('.ad-form');
+const filtersForm = document.querySelector('.map__filters');
+
+const deactivateFormElements = (form) => Array.from(form.elements).forEach((element) => {
+  if (element.tagName !== 'FIELDSET') {
+    element.disabled = true;
+  }
+});
+
+const activateFormElements = (form) => Array.from(form.elements).forEach((element) => {
+  element.disabled = false;
+});
+
+const deactivateForms = () => {
+  filtersForm.classList.add('map__filters--disabled');
+  newAdvertisementForm.classList.add('ad-form--disabled');
+  deactivateFormElements(filtersForm);
+  deactivateFormElements(newAdvertisementForm);
+};
+
+const activateForms = () => {
+  filtersForm.classList.remove('map__filters--disabled');
+  newAdvertisementForm.classList.remove('ad-form--disabled');
+  activateFormElements(filtersForm);
+  activateFormElements(newAdvertisementForm);
+};
+
+const initFormModule = () => {
+  deactivateForms();
+  activateForms();
+};
+
+export { initFormModule };
+
--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 import { renderAdvertisementCard } from './similar-advertisements.js';
+import { initFormModule } from './form.js';
 
 renderAdvertisementCard();
+initFormModule();
