e04514b94a716e78aec8bac577907368ec4ad155
CendresChaudes <romqaaa1337@gmail.com>
2023-04-09 22:14:57 +0300

    feat: добавляет обработку ситуаций с пустыми данными

--- js/similar-advertisements.js
+++ js/similar-advertisements.js
@@ -1,6 +1,8 @@
 const advertisementCardTemplate = document.querySelector('#card').content.querySelector('.popup');
 
-const getTypeHousing = (type) => {
+const getPriceInfo = (price) => price ? `${price} <span>₽/ночь</span>` : '';
+
+const getTypeInfo = (type) => {
   switch (type) {
     case 'palace':
       return 'Дворец';
@@ -13,13 +15,32 @@ const getTypeHousing = (type) => {
     case 'hotel':
       return 'Отель';
     default:
-      return 'Тип жилья не найден!';
+      return '';
   }
 };
 
-const renderFeatures = (element, features) => {
-  const featuresList = element.querySelectorAll('.popup__feature');
+const getCapacityInfo = (rooms, guests) => {
+  if (rooms !== 'undefined' && guests !== 'undefined') {
+    return `${rooms} комнаты для ${guests} гостей`;
+  }
 
+  return '';
+};
+
+const getCheckinCheckoutInfo = (checkin, checkout) => {
+  if (checkin !== 'undefined' && checkout !== 'undefined') {
+    return `Заезд после ${checkin}, выезд до ${checkout}`;
+  }
+
+  return '';
+};
+
+const renderFeatures = (element, features) => {
+  if (!features) {
+    return;
+  }
+
+  const featuresList = element.querySelectorAll('.popup__feature');
   featuresList.forEach((item) => {
     const hasFeature = features.some((feature) => item.classList.contains(`popup__feature--${feature}`));
     if (!hasFeature) {
@@ -33,6 +54,11 @@ const renderPhotos = (element, photos) => {
   const photosContainer = element.querySelector('.popup__photos');
   photosContainer.innerHTML = '';
 
+  if (!photos) {
+    photosContainer.append('');
+    return;
+  }
+
   photos.forEach((photo) => {
     const popupPhoto = popupPhotoTemplate.cloneNode(true);
     popupPhoto.src = photo;
@@ -42,14 +68,14 @@ const renderPhotos = (element, photos) => {
 
 const createAdvertisementCard = ({author, offer}) => {
   const advertisementCard = advertisementCardTemplate.cloneNode(true);
-  advertisementCard.querySelector('.popup__avatar').src = author.avatar;
-  advertisementCard.querySelector('.popup__title').textContent = offer.title;
-  advertisementCard.querySelector('.popup__text--address').textContent = offer.address;
-  advertisementCard.querySelector('.popup__text--price').innerHTML = `${offer.price} <span>₽/ночь</span>`;
-  advertisementCard.querySelector('.popup__type').textContent = getTypeHousing(offer.type);
-  advertisementCard.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
-  advertisementCard.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
-  advertisementCard.querySelector('.popup__description').textContent = offer.description;
+  advertisementCard.querySelector('.popup__avatar').src = author.avatar ?? '';
+  advertisementCard.querySelector('.popup__title').textContent = offer.title ?? '';
+  advertisementCard.querySelector('.popup__text--address').textContent = offer.address ?? '';
+  advertisementCard.querySelector('.popup__text--price').innerHTML = getPriceInfo(offer.price);
+  advertisementCard.querySelector('.popup__type').textContent = getTypeInfo(offer.type);
+  advertisementCard.querySelector('.popup__text--capacity').textContent = getCapacityInfo(offer.rooms, offer.guests);
+  advertisementCard.querySelector('.popup__text--time').textContent = getCheckinCheckoutInfo(offer.checkin, offer.checkout);
+  advertisementCard.querySelector('.popup__description').textContent = offer.description ?? '';
   renderFeatures(advertisementCard, offer.features);
   renderPhotos(advertisementCard, offer.photos);
 
